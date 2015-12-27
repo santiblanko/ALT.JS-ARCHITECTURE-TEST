3ebafec2daa2b882497c606c803561ce58a9b317
Santiago Blanco <santiago.blanco.vilchez@gmail.com>
2015-12-26 19:39:06 -0500

    Added styles and set initial components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
-
+import MessageList from './MessageList.jsx'
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import basicTheme from '../themes/basicTheme';
+import AppBar from 'material-ui/lib/app-bar';
+import LinearProgress from 'material-ui/lib/linear-progress';
+import ChannelList from './ChannelList.jsx';
+import MessageBox from './MessageBox.jsx';
 class App extends React.Component {
+
 	constructor(){
 		super()
-		this.state = {
-			messages:[
-			'hi there hows arssde you?',
-			'I am fines, and you?'
-			]
-		}
 	}
 
 	render(){
 		//debugger;
-		var messagesNodes = this.state.messages.map((message) => {
-			return (
-				<div>{message}</div>
-			)
-		})
-
 		return (
-			<div>{messagesNodes}</div>
+			<div>
+				<LinearProgress mode="determinate" value={100} size={5} color={"black"} />
+				<AppBar title="Awesome Chat App" />
+		        <div style={{
+		          display: 'flex',
+		          flexFlow: 'row wrap',
+		          maxWidth: 1200,
+		          width: '100%',
+		          margin: '30px auto 30px'
+		        }}>
+					<ChannelList/>
+					<MessageList />
+				</div>
+
+				<MessageBox/>
+
+			</div>
 		)
 	}
 
+	getChildContext() {
+	    return {
+	      muiTheme: ThemeManager.getMuiTheme(basicTheme),
+	    };
+  }
+
 }
+
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
 
 export default App;
